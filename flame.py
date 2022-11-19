0f42aac8b972bf30fa14a6f1defc54cc4d77e916
nersan <narasuke114@icloud.com>
2022-11-19 09:39:12 +0900

    moke

--- flame.py
+++ flame.py
@@ -8,3 +8,4 @@ class Flame:
        self.end_y = end_y
        self.color = color
        self.id = self.canvas.create_rectangle(self.start_x, self.start_y, self.end_x, self.end_y, outline=self.color, fill='', width=width)
+       print(32)
