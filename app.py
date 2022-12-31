037369758d53ffe09004c24f60634f289b8ffac4
nersan <narasuke114@icloud.com>
2022-12-31 15:43:37 +0900

    oval can't move. help me

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import tkinter as tk
 from oval import Oval
 from flame import Flame
 #  10.8m*10.8m module 90cm 
-INIT_WIDTH = 3
-INIT_HEIGHT = 2
-INIT_AREASIZE = 10
+# INIT_WIDTH = 3
+# INIT_HEIGHT = 2
+# INIT_AREASIZE = 10
 MAGNIFICATION = 2500
 OVAL_QUANTITY_LIMIT = 100
 IS_MOVING = False   # 楕円が移動中かどうか(移動中: True, 楕円作っている: False)
@@ -38,35 +38,61 @@ class App(tk.Frame):
                 self.canvas.create_line(a, 0, a, 2000, dash=(1, 5))
                 self.canvas.create_line(0, b, 2000, b, dash=(1, 5))
         
-        # 面積のテキストボックス
-        self.areasize_lbl = tk.Label(text='面積')
-        self.areasize_lbl.place(x=10, y=0)
-        self.areasize_box = tk.Entry(width=10)
-        self.areasize_box.place(x=50, y=0)
-        self.areasize_box.insert(tk.END, INIT_AREASIZE)
-        self.areasize = self.areasize_box.get()
+        # # 面積のテキストボックス
+        # self.areasize_lbl = tk.Label(text='面積')
+        # self.areasize_lbl.place(x=10, y=0)
+        # self.areasize_box = tk.Entry(width=10)
+        # self.areasize_box.place(x=50, y=0)
+        # self.areasize_box.insert(tk.END, INIT_AREASIZE)
+        # self.areasize = self.areasize_box.get()
 
-        # 縦横比のテキストボックス
-        # 縦
-        self.height_relative_lbl = tk.Label(text='縦')
-        self.height_relative_lbl.place(x=150, y=0)
-        self.height_relative_box = tk.Entry(width=10)
-        self.height_relative_box.place(x=190, y=0)
-        self.height_relative_box.insert(tk.END, INIT_HEIGHT)
-        self.height_relative = self.height_relative_box.get()
+        # # 縦横比のテキストボックス
+        # # 縦
+        # self.height_relative_lbl = tk.Label(text='縦')
+        # self.height_relative_lbl.place(x=150, y=0)
+        # self.height_relative_box = tk.Entry(width=10)
+        # self.height_relative_box.place(x=190, y=0)
+        # self.height_relative_box.insert(tk.END, INIT_HEIGHT)
+        # self.height_relative = self.height_relative_box.get()
 
-        # 横
-        self.width_relative_lbl = tk.Label(text='横')
-        self.width_relative_lbl.place(x=290, y=0)
-        self.width_relative_box = tk.Entry(width=10)
-        self.width_relative_box.place(x=330, y=0)
-        self.width_relative_box.insert(tk.END, INIT_WIDTH)
-        self.width_relative = self.width_relative_box.get()
+        # # 横
+        # self.width_relative_lbl = tk.Label(text='横')
+        # self.width_relative_lbl.place(x=290, y=0)
+        # self.width_relative_box = tk.Entry(width=10)
+        # self.width_relative_box.place(x=330, y=0)
+        # self.width_relative_box.insert(tk.END, INIT_WIDTH)
+        # self.width_relative = self.width_relative_box.get()
         
-        #　更新ボタン
-        self.areasize_update_button = tk.Button(self, text='更新', command=self.update_areasize)
+        # #　更新ボタン
+        # self.areasize_update_button = tk.Button(self, text='更新', command=self.update_areasize)
+        # self.areasize_update_button.place(x=435, y=0)
+
+        # 移動ボタン
+        self.areasize_update_button = tk.Button(self, text='移動', command=self.press_move)
+        self.areasize_update_button.place(x=200, y=0)
+
+        
+
+        #　面積簡易変更ボタン
+        self.areasize_update_button = tk.Button(self, text='大', command=self.big_areasize)
         self.areasize_update_button.place(x=435, y=0)
-        
+
+        self.areasize_update_button = tk.Button(self, text='中', command=self.middle_areasize)
+        self.areasize_update_button.place(x=435, y=25)
+
+        self.areasize_update_button = tk.Button(self, text='小', command=self.small_areasize)
+        self.areasize_update_button.place(x=435, y=50)
+
+        #　形簡易変更ボタン
+        self.areasize_update_button = tk.Button(self, text='縦', command=self.y_shape)
+        self.areasize_update_button.place(x=500, y=0)
+
+        self.areasize_update_button = tk.Button(self, text='中', command=self.middle_shape)
+        self.areasize_update_button.place(x=500, y=25)
+
+        self.areasize_update_button = tk.Button(self, text='横', command=self.x_shape)
+        self.areasize_update_button.place(x=500, y=50)
+
         #　楕円ボタン
         self.oval_button = tk.Button(self, text='楕円', command=self.press_oval)
         self.oval_button.place(x=545, y=0)
@@ -128,10 +154,34 @@ class App(tk.Frame):
 
         #　FlameクラスとAppクラスのキャンバスの統合
         Flame.canvas = self.canvas
-    
+
+    # 「移動」ボタンが押された時
+    def press_move(self):
+        global IS_MOVING
+        IS_MOVING = True
+        for o in self.oval:
+            if o.deleted == False:
+                o.bind_move()
+
+    def big_areasize(self):
+        self.areasize = 20
+    def middle_areasize(self):
+        self.areasize = 10
+    def small_areasize(self):
+        self.areasize = 5
+    def y_shape(self):
+        self.height_relative = 3
+        self.width_relative = 1
+    def middle_shape(self):
+        self.height_relative = 2
+        self.width_relative = 3
+    def x_shape(self):
+        self.height_relative = 1
+        self.width_relative = 3
 
     #　最終ボタンが押された時、１階と２階の最終的な平面図をシンプルに見せる
     def last(self):
+        # 余計なものの削除
         for i in self.room2:
             self.canvas.delete(i.id)
         for u in self.room4:
@@ -139,7 +189,7 @@ class App(tk.Frame):
         self.canvas.delete(self.stairs2[1].id)
         self.canvas.delete(self.flame[1].id)
         
-        
+        # 重ねた部屋を２階の位置に移動（新たに製造）
         for e in self.room4:
             start_x = e.start_x + DIFFERENCE
             start_y = e.start_y
@@ -147,13 +197,14 @@ class App(tk.Frame):
             end_y = e.end_y
             room = Flame(start_x, start_y, end_x, end_y, 'black', 5)
             self.room5.append(room)
-        
+        # 階段
         s_x = self.stairs2[0].start_x + DIFFERENCE
         s_y = self.stairs2[0].start_y
         e_x = self.stairs2[0].end_x + DIFFERENCE
         e_y = self.stairs2[0].end_y
         stairs = Flame(s_x, s_y, e_x, e_y, 'blue', 5)
 
+        # 外枠
         left_most = None
         right_most = None
         up_most = None
@@ -165,10 +216,7 @@ class App(tk.Frame):
             down_most = a.end_y if down_most is None or a.end_y > down_most else down_most
         flame = Flame(left_most, up_most, right_most, down_most, 'black', 5)
 
-        pass
-
-
-    #　ストレッチボタンが押された時、重ねた部屋の大きさを合わせる、２階を１階に合わせる
+    #　ストレッチボタンが押された時、重ねた部屋の大きさを合わせる（拡大、縮小）、２階を１階に合わせる
     def stretch(self):
         start_x1 = self.flame[0].start_x 
         start_y1 = self.flame[0].start_y
@@ -182,6 +230,7 @@ class App(tk.Frame):
         difference_start_y = start_y2 - start_y1
         difference_end_x = end_x2 - end_x1
         difference_end_y = end_y2 - end_y1
+        # 外枠に部屋が接している場合はそれ以上外に出ないようしたい
         for ro in self.room3:
             if ro.start_x == start_x1:
                 start_x3 = start_x1
@@ -202,11 +251,9 @@ class App(tk.Frame):
             
             room3 = Flame(start_x3, start_y3, end_x3, end_y3, 'red', 2)
             self.room4.append(room3)
-
             self.canvas.delete(ro.id)
 
         self.canvas.delete(self.flame2[0].id)
-
 
     #　階段ボタンが押された時
     def stairs(self):
@@ -267,7 +314,6 @@ class App(tk.Frame):
             end_y2 = (ro.end_y - self.stairs2[1].start_y + self.stairs2[0].start_y)
             roo = Flame(start_x2, start_y2, end_x2, end_y2, 'green', 3)
             self.room3.append(roo)
-
 
     # 外枠ボタンが押されたとき
     def create_flame(self):
@@ -349,10 +395,7 @@ class App(tk.Frame):
                 ov = Oval(center_x, center_y, center_x-dist_x, center_y-dist_y, center_x+dist_x, center_y+dist_y, size, width, height, self.x, self.y)
                 self.oval.append(ov)
                 self.oval_flame_creation.append(ov)
-
-                # self.oval[floor].append(ov)
             self.canvas.delete('error_message')
         except ValueError:
             self.canvas.create_text(100, 100, text='数値を入力してください', fill='red', tags='error_message')
     
-
